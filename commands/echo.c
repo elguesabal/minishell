6e45d9fcff93b86f587b11c0496ea09e88bbe441
elguesabal <joseelguesabal@gmail.com>
2024-04-08 19:09:16 -0300

    inserindo a flag -n no echo

--- commands/echo.c
+++ commands/echo.c
@@ -2,9 +2,19 @@
 
 void	echo(char *comand)	// PENSAR MELHOR SOBRE OQ ESSA FUNCAO DEVE RETORNAR E COMO TRATAR ERROS
 {
+	char flag;
+
+	flag = '\n';
 	while (*comand && *comand != ' ' && *comand != '	')
 		comand++;
 	while (*comand && (*comand == ' ' || *comand == '	'))
 		comand++;
-	printf("%s\n", comand);
+	if (comand[0] == '-' && comand[1] == 'n')
+	{
+		flag = '\0';
+		comand += 2;
+		while (*comand && (*comand == ' ' || *comand == '	'))
+			comand++;
+	}
+	printf("%s%c", comand, flag);
 }
