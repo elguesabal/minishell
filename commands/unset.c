97cb0a68cddd0b6459702b48fd735fc97c217bcd
elguesabal <joseelguesabal@gmail.com>
2024-04-24 17:57:18 -0300

    acabei testando algumas possibilidades e nao fazendo nada de relevante estou fzndo o commit para nao haver conclito no git por causa de arquivos modificados sem registro

--- commands/unset.c
+++ commands/unset.c
@@ -1,43 +1,46 @@
 #include "../minishell.h"
 
+// char	*search_and_return_previous(t_str **no, char *str) // SEMELHANTE A search_variable_list() MAS RETORNA O NO ANTERIOR
+// {
+// 	t_str	*assist;
+//     t_str   *previous;
+
+// 	assist = *no;
+// 	while (assist)
+// 	{
+// 		if (compare_variable(assist->str, str))
+// 			return (assist->str);
+// 		assist = assist->next;
+// 	}
+// 	return (NULL);
+// }
+
+// char    **remove(char **env, char *str)
+// {
+
+//     if ()
+//     {
+
+//     }
+// }
+
 char	**unset(char **argv, t_str **env_list, char **env)
 {
     (void)argv;
     (void)env_list;
     (void)env;
-    printf("testando unset");
-    return (NULL);
 
-    // int i;
+    int i;
     // t_str   *assist;
     // t_str   *previous_assist;
 
-    // i = 1;
-	// while (argv[i])
-    // {
-    //     assist = *env_list;
-    //     while (assist)
-    //     {
-    //         previous_assist = assist;
-    //         assist = assist->next;
-    //     }
+    i = 1;
+	while (argv[i])
+    {
+        // printf("testando unset: %s\n", argv[i]);
 
 
-    //     assist = *env_list;
-    //     while (assist)
-    //     {
-    //         if (assist->next && assist->next->str)
-    //         {
-                
-    //         }
-    //     }
-
-
-
-
-    //     i++;
-    // }
+        i++;
+    }
+    return (NULL);
 }
-
-
-// char **array = { "teste", "123", NULL, NULL };
--- minishell.c
+++ minishell.c
@@ -32,12 +32,12 @@ void	analyze(char **argv, t_str **env_list)
 	i = 1; // TEM Q COMECAR A VERIFICAR APARTIR DO SEGUNDO ARGUMENTO PQ O PRIMEIRO E O COMANDO
 	while (argv[i])
 	{
-		argv[i] = environment_variable(argv[i], env_list); // $
-		// >
-		// >>
-		// |
-		// <
-		// <<
+		/* $ */ argv[i] = environment_variable(argv[i], env_list);
+		/* > */
+		/* >> */
+		/* | */
+		/* < */
+		/* << */
 
 		i++;
 	}
